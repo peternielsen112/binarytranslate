022ba30220cf6d9fb9cbf3bef597d8dd2b7b68da
Peter Nielsen <dirigo112@gmail.com>
2020-04-27 19:31:13 -0700

    Initialize
    Adding main.py, likely the only file for the repository.

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+
+input = sys.argv[1]
+
+if input == '1':
+    print('0000001')
+elif input == '2':
+    print('0000010')
+elif input == '3':
+    print('0000011')
+elif input == '4':
+    print('0000100')
+elif input == '5':
+    print('0000101')
+elif input == '6':
+    print('0000110')
+elif input == '7':
+    print('0000111')
+elif input == '8':
+    print('0001000')
+elif input == '9':
+    print('0001001')
+elif input == '10':
+    print('0001010')
+elif input == '11':
+    print('0001011')
+elif input == '12':
+    print('0001100')
+elif input == '13':
+    print('0001101')
+elif input == 'E':
+    print('1000101')
+elif input == 'F':
+    print('1000110')
+elif input == 'G':
+    print('1000111')
+elif input == 'H':
+    print('1001000')
+elif input == 'I':
+    print('1001001')
+elif input == 'a':
+    print('0000000')
+elif input == 'b':
+    print('0000000')
+else:
+    print('Sorry, the number or character you have typed is not available to be translated at this time.')
